b96e68889062fb09e9d8e6e3648eb2841594de04
GuillaumePOREZ72 <guillaume.porez@oclock.school>
2024-10-28 15:15:36 +0100

    maj taskInput

--- todo-list-react/task-flow/src/components/taskInput/taskInput.jsx
+++ todo-list-react/task-flow/src/components/taskInput/taskInput.jsx
@@ -1,6 +1,39 @@
-// Ce composant est utilisé pour afficher le champ de saisie de tâche.
+import TaskList from "../taskList/taskList";
+import styles from "./TaskInput.module.css";
+import { useState } from "react";
 
-export const TaskInput = () => {
-  return <div></div>;
+const TaskInput = ({ addTask }) => {
+  const [taskTitle, setTaskTitle] = useState("");
+
+  const handleInputChange = (e) => {
+    setTaskTitle(e.target.value);
+  };
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    if (taskTitle.trim()) {
+      addTask(taskTitle);
+      setTaskTitle("");
+    }
+  };
+
+  return (
+    <div className={`box ${styles.element}`}>
+      <h2 className={styles.title}>Ajoute ta prochaine tâche</h2>
+      <form className={styles.container} onSubmit={handleAddTask}>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Ecrire une tâche"
+          onChange={handleInputChange}
+          value={taskTitle}
+        />
+        <button className="button-primary" type="submit">
+          Ajouter
+        </button>
+      </form>
+    </div>
+  );
 };
-      
+
+export default TaskInput;
